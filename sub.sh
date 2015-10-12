f67b2b32a7a597731d3ce8d0f18c1bdea4a933b1
koji3 <jose.linares.1993@gmail.com>
2015-10-12 11:53:14 +0200

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -3,8 +3,8 @@
 
 for file in $@ ; do
 	nombre=$(echo $file | rev | cut -c 5- | rev)
-	#subliminal -les "$file"
-	if [[ $(test -e "$nombre.en.srt") = '' ]] ; then 
+	subliminal -les "$file"
+	if [[ $(test -e "$nombre.es.srt") = '' ]] ; then 
 		mencoder "$file" -ovc xvid -oac mp3lame -lameopts abr:br=192 -xvidencopts pass=2:bitrate=-700000 -sub "$nombre.en.srt" -subcp utf-8 -subfont-text-scale 2.8 -o "$nombre.avi"
 	fi
 
