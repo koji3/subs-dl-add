6c595dc2f1c381e843c16738aa4392b503623c80
koji3 <jose.linares.1993@gmail.com>
2015-10-12 11:52:05 +0200

    Primer commit

---
+++ sub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+for file in $@ ; do
+	nombre=$(echo $file | rev | cut -c 5- | rev)
+	#subliminal -les "$file"
+	if [[ $(test -e "$nombre.en.srt") = '' ]] ; then 
+		mencoder "$file" -ovc xvid -oac mp3lame -lameopts abr:br=192 -xvidencopts pass=2:bitrate=-700000 -sub "$nombre.en.srt" -subcp utf-8 -subfont-text-scale 2.8 -o "$nombre.avi"
+	fi
+
+done
+
+
