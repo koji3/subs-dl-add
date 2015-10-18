0bdbe97d6e86e043edf0365a9bbb80582d184737
rafa leyva ruiz <rafaelleru@users.noreply.github.com>
2015-10-18 22:45:03 +0200

    Update sub.sh
    Cambiada la errata de la opcion subliminal que no funcionaba ya que ponia -les y lo correcto es -l es

--- sub.sh
+++ sub.sh
@@ -3,7 +3,7 @@
 
 for file in $@ ; do
 	nombre=$(echo $file | rev | cut -c 5- | rev)
-	subliminal -les "$file"
+	subliminal -l es "$file"
 	if [[ $(test -e "$nombre.es.srt") = '' ]] ; then 
 		mencoder "$file" -ovc xvid -oac mp3lame -lameopts abr:br=192 -xvidencopts pass=2:bitrate=-700000 -sub "$nombre.es.srt" -subcp utf-8 -subfont-text-scale 2.8 -o "$nombre.avi"
 	fi
